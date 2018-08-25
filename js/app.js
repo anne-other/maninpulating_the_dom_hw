b5990bf8c7a150938ea0eb52a1c47131c50250c1
anne-other <anne.c.ince@gmail.com>
2018-08-25 22:38:46 +0100

    refactoring code to add helper and getting it to work on local storage

--- js/app.js
+++ js/app.js
@@ -1,69 +1,66 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("#form");
   form.addEventListener('submit', handleFormSubmit);
-//
-//   renderList();
+
+    renderList();
 });
-//
-// const localSongs = function () {
-//   JSON.parse(localStorage.getItem("songs"));
-// }
-//
-// const getList = function () {
-//   if ((localSongs) !== null){
-//     return
-//     localSongs;
-//   }
-//   else {
-//     return [];
-//   }
-// };
-//
+
+const localSongs = function () {
+  return JSON.parse(localStorage.getItem("songs"));
+}
+
+const getList = function () {
+  let localSong = localSongs()
+  if (localSong !== null){
+    return localSong;
+  }
+  else {
+    return [];
+  }
+};
+
+const createLi = function (liItem, value) {
+  const itemLi = document.createElement("li");
+  itemLi.textContent = `${liItem}: ${value}`;
+
+  return itemLi;
+}
+
 const handleFormSubmit = function (event) {
   event.preventDefault();
-  const resultForm = document.querySelector("#song-list");
-  const songUl = document.createElement('ul');
-  const songTitleLi = document.createElement('li');
-  songTitleLi.textContent = `Song Title: ${event.target.song_title.value}`;
-  songUl.appendChild(songTitleLi);
-  resultForm.appendChild(songUl);
-//   rockSongList = [];
-//   const newSong = {
-//     song_title: event.target.song_title.value,
-//     band_name: event.target.band_name.value,
-//     genre: event.target.genre.value
-  }
-//
-//   rockSongList.push(newSong);
-//
-//   localStorage.setItem("songs", JSON.stringify(rockSongList));
-//   renderList();
-//   event.target.reset();
-// }
-//
-// const createLi = function (liItem, song) {
-//   const itemLi = document.createElement("li");
-//   itemLi.textContent = `${liItem}: ${song["liItem"]}`;
-//
-//   return itemLi;
-// }
-//
-// const buildList = function (song) {
-//   const songUl = document.createElement("ul");
-//   songUl.forEach((liItem) => {
-//     liItem = createLi(liItem);
-//     songUl.appendChild(liItem);
-//
-//     return songUl
-//   })
-// }
-//
-// const renderList = function () {
-//   const songDiv = document.querySelector("#song-list");
-//   songDiv.innerHTML = "";
-//   const songList = getList();
-//   songList.forEach((song) => {
-//     songUl = buildList(song);
-//     songDiv.appendChild(songUl);
-//   });
-// }
+  rockSongList = getList();
+
+  song = {
+    "Song Title": event.target.song_title.value,
+    "Band Name": event.target.band_name.value,
+    "Genre": event.target.genre.value
+  };
+
+  rockSongList.push(song);
+
+  localStorage.setItem("songs", JSON.stringify(rockSongList));
+  renderList();
+  event.target.reset();
+}
+
+const buildList = function (song) {
+  const songUl = document.createElement("ul");
+
+  for(var liItem in song) {
+    let listItem = createLi(liItem, song[liItem]);
+    songUl.appendChild(listItem);
+  };
+
+  return songUl
+}
+
+const renderList = function () {
+  const songDiv = document.querySelector("#song-list");
+  songDiv.innerHTML = "";
+
+  const songList = getList();
+  songList.forEach((song) => {
+    songUl = buildList(song);
+    songDiv.appendChild(songUl);
+  });
+}
