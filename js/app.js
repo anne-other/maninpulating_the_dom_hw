f8111d8c6221530cd3380396f8a34a9046882d5b
anne-other <anne.c.ince@gmail.com>
2018-08-25 16:02:26 +0100

    a new approach

--- js/app.js
+++ js/app.js
@@ -1,47 +1,63 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("#form");
   form.addEventListener('submit', handleFormSubmit);
-
-  renderList();
+//
+//   renderList();
 });
-
-const localSongs = function () {
-  JSON.parse(localStorage.getItem("songs");
-}
-
-const getList = function () {
-  if (localSongs) !== null){
-    return
-    localSongs;
+//
+// const localSongs = function () {
+//   JSON.parse(localStorage.getItem("songs"));
+// }
+//
+// const getList = function () {
+//   if ((localSongs) !== null){
+//     return
+//     localSongs;
+//   }
+//   else {
+//     return [];
+//   }
+// };
+//
+const handleFormSubmit = function (event) {
+  event.preventDefault();
+//   rockSongList = [];
+//   const newSong = {
+//     song_title: event.target.song_title.value,
+//     band_name: event.target.band_name.value,
+//     genre: event.target.genre.value
   }
-  else {
-    return [];
-  }
-};
-
-const createLi = function (liItem, song) {
-  const(liItme)Li = document.createElement("li");
-  (liItem)Li.textContent = `${liItem}: ${song["liItem"]}`;
-
-  return (liItem)Li;
-}
-
-const buildList = function (song) {
-  const songUl = document.createElement("ul");
-  song.forEach((liItem) => {
-    liItem = createLi(liItem);
-    songUl.appendChild(liItem);
-    
-    return songUl
-  })
-}
-
-const renderList = function () {
-  const songDiv = document.querySelector("#song-list");
-  songDiv.innerHTML = "";
-  const songList = getList();
-  songList.forEach((song) => {
-    songUl = buildList(song);
-    songDiv.appendChild(songUl);
-  });
-}
+//
+//   rockSongList.push(newSong);
+//
+//   localStorage.setItem("songs", JSON.stringify(rockSongList));
+//   renderList();
+//   event.target.reset();
+// }
+//
+// const createLi = function (liItem, song) {
+//   const itemLi = document.createElement("li");
+//   itemLi.textContent = `${liItem}: ${song["liItem"]}`;
+//
+//   return itemLi;
+// }
+//
+// const buildList = function (song) {
+//   const songUl = document.createElement("ul");
+//   songUl.forEach((liItem) => {
+//     liItem = createLi(liItem);
+//     songUl.appendChild(liItem);
+//
+//     return songUl
+//   })
+// }
+//
+// const renderList = function () {
+//   const songDiv = document.querySelector("#song-list");
+//   songDiv.innerHTML = "";
+//   const songList = getList();
+//   songList.forEach((song) => {
+//     songUl = buildList(song);
+//     songDiv.appendChild(songUl);
+//   });
+// }
